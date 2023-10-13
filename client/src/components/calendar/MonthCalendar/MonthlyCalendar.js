a90f9eaf80092f2f6fdd9ea45cf9f0d3bf777da6
Suchith Kumar G M <suchithkumargm@gmail.com>
2023-10-13 19:15:42 +0530

    fixed frontend bug

--- client/src/components/calendar/MonthCalendar/MonthlyCalendar.js
+++ client/src/components/calendar/MonthCalendar/MonthlyCalendar.js
@@ -1,79 +1,82 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import GenerateMonthlyCalendar from './GenerateMonthlyCalendar';
-import './MonthlyCalendar.css'
+import './MonthlyCalendar.css';
 
-class MonthlyCalendar extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            currentYear: new Date().getFullYear(),
-            currentMonthIndex: new Date().getMonth(),
-            monthNames: [
-                "January", "February", "March", "April", "May", "June",
-                "July", "August", "September", "October", "November", "December"
-            ],
-            holidays: [],
-        };
+const MonthlyCalendar = () => {
+    const [currentYear, setCurrentYear] = useState(new Date().getFullYear());
+    const [currentMonthIndex, setCurrentMonthIndex] = useState(new Date().getMonth());
+    const monthNames = [
+        "January", "February", "March", "April", "May", "June",
+        "July", "August", "September", "October", "November", "December"
+    ];
+    const [holidays, setHolidays] = useState([]);
+    const [buttonClick, setButtonClick] = useState(false);
+
+    const handleButtonClick = () => {
+        setButtonClick(!buttonClick)
     }
 
-    showPrevYear = () => {
-        this.setState({ currentYear: this.state.currentYear - 1 });
+    const showPrevYear = () => {
+        setCurrentYear(currentYear - 1);
     }
 
-    showNextYear = () => {
-        this.setState({ currentYear: this.state.currentYear + 1 });
+    const showNextYear = () => {
+        setCurrentYear(currentYear + 1);
     }
 
-    showPrevMonth = () => {
-        if (this.state.currentMonthIndex > 0) {
-            this.setState({ currentMonthIndex: this.state.currentMonthIndex - 1 });
+    const showPrevMonth = () => {
+        if (currentMonthIndex > 0) {
+            setCurrentMonthIndex(currentMonthIndex - 1);
         } else {
-            this.setState({ currentMonthIndex: 11, currentYear: this.state.currentYear - 1 });
+            setCurrentMonthIndex(11);
+            setCurrentYear(currentYear - 1);
         }
     }
 
-    showNextMonth = () => {
-        if (this.state.currentMonthIndex < 11) {
-            this.setState({ currentMonthIndex: this.state.currentMonthIndex + 1 });
+    const showNextMonth = () => {
+        if (currentMonthIndex < 11) {
+            setCurrentMonthIndex(currentMonthIndex + 1);
         } else {
-            this.setState({ currentMonthIndex: 0, currentYear: this.state.currentYear + 1 });
+            setCurrentMonthIndex(0);
+            setCurrentYear(currentYear + 1);
         }
     }
 
-    updateHolidays = (newHoliday) => {
-        this.setState({ holidays: [...this.state.holidays, newHoliday] });
+    const updateHolidays = (newHoliday) => {
+        // Clone the current holidays state and add the new holiday
+        setHolidays([...holidays, newHoliday]);
+        console.log(holidays)
     }
 
-    componentDidMount() {
+    useEffect(() => {
         // Fetch the list of holidays from the server
         fetch('http://localhost:5000/calendar/getholidays')
             .then((response) => response.json())
             .then((data) => {
-                this.setState({ holidays: data });
+                setHolidays(data);
             })
             .catch((error) => {
                 console.error('Error while fetching holidays:', error);
             });
-    }
+    }, [buttonClick]);
 
-    render() {
-        return (
-            <div className="inner-month-container">
-                <GenerateMonthlyCalendar
-                    currentYear={this.state.currentYear}
-                    currentMonthIndex={this.state.currentMonthIndex}
-                    showPrevYear={this.showPrevYear}
-                    showNextYear={this.showNextYear}
-                    showPrevMonth={this.showPrevMonth}
-                    showNextMonth={this.showNextMonth}
-                    monthNames={this.state.monthNames}
-                    holidays={this.state.holidays}
-                    updateHolidays={this.updateHolidays}
-                />
-            </div>
-        );
-    }
+    return (
+        <div className="inner-month-container">
+            <GenerateMonthlyCalendar
+                currentYear={currentYear}
+                currentMonthIndex={currentMonthIndex}
+                showPrevYear={showPrevYear}
+                showNextYear={showNextYear}
+                showPrevMonth={showPrevMonth}
+                showNextMonth={showNextMonth}
+                monthNames={monthNames}
+                holidays={holidays}
+                updateHolidays={updateHolidays}
+                handleButtonClick={handleButtonClick}
+            />
+        </div>
+    );
 }
 
 export default MonthlyCalendar;
--- client/src/components/calendar/MonthCalendar/generateMonthlyCalendar.js
+++ client/src/components/calendar/MonthCalendar/generateMonthlyCalendar.js
@@ -1,20 +1,29 @@
-import React, { Component } from 'react';
+import React, { useState} from 'react';
 
-class GenerateMonthlyCalendar extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            selectedDate: new Date(),
-            isHolidayState: false,
-        };
-    }
+const GenerateMonthlyCalendar = (props) => {
+    const {
+        currentYear,
+        currentMonthIndex,
+        showPrevYear,
+        showNextYear,
+        showPrevMonth,
+        showNextMonth,
+        monthNames,
+        holidays,
+        updateHolidays,
+        handleButtonClick
+    } = props;
 
-    handleDateClick = (day, month, year) => {
+    const [selectedDate, setSelectedDate] = useState(new Date());
+
+    const handleDateClick = (day, month, year) => {
         const newSelectedDate = new Date(Date.UTC(year, month, day));
-        this.setState({ selectedDate: newSelectedDate });
+        setSelectedDate(newSelectedDate);
+        handleButtonClick();
 
         const formattedDate = `${year}-${(month + 1).toString().padStart(2, '0')}-${day.toString().padStart(2, '0')}`;
 
+        // Send a POST request to check if it's a holiday
         fetch('http://localhost:5000/calendar/checkholiday', {
             method: 'POST',
             headers: {
@@ -28,9 +37,9 @@ class GenerateMonthlyCalendar extends Component {
         })
             .then((response) => response.json())
             .then((data) => {
-                this.setState({ isHolidayState: data.workingDay });
                 if (!data.workingDay) {
-                    this.props.updateHolidays(formattedDate);
+                    // If it's a holiday, call the updateHolidays function
+                    updateHolidays(formattedDate);
                 }
             })
             .catch((error) => {
@@ -38,100 +47,96 @@ class GenerateMonthlyCalendar extends Component {
             });
     }
 
-    isHoliday = (day, month, year) => {
+    const isHoliday = (day, month, year) => {
         const formattedDate = `${year}-${(month + 1).toString().padStart(2, '0')}-${day.toString().padStart(2, '0')}`;
-        return this.props.holidays.includes(formattedDate);
+        return holidays.includes(formattedDate);
     }
 
-    render() {
-        const { currentYear, currentMonthIndex, showPrevYear, showNextYear, showPrevMonth, showNextMonth, monthNames } = this.props;
-        const { selectedDate } = this.state;
+    const currentMonth = currentMonthIndex;
+    const currentDate = new Date(currentYear, currentMonth, 1);
+    const daysInMonth = new Date(currentYear, currentMonthIndex + 1, 0).getDate();
+    const firstDayIndex = currentDate.getDay();
+    const today = new Date();
 
-        const currentDate = new Date(currentYear, currentMonthIndex, 1);
-        const daysInMonth = new Date(currentYear, currentMonthIndex + 1, 0).getDate();
-        const firstDayIndex = currentDate.getDay();
-        const today = new Date();
+    const calendarRows = [];
+    let dayCells = [];
 
-        const calendarRows = [];
-        let dayCells = [];
+    for (let i = 0; i < firstDayIndex; i++) {
+        dayCells.push(<td key={i}></td>);
+    }
 
-        for (let i = 0; i < firstDayIndex; i++) {
-            dayCells.push(<td key={i}></td>);
-        }
+    for (let day = 1; day <= daysInMonth; day++) {
+        const dayOfWeek = (firstDayIndex + day - 1) % 7;
+        const isSunday = dayOfWeek === 0;
+        const isToday = currentDate.getDate() === today.getDate() && currentDate.getMonth() === today.getMonth() && currentDate.getFullYear() === today.getFullYear();
+        const isSelected = currentDate.toDateString() === selectedDate.toDateString();
 
-        for (let day = 1; day <= daysInMonth; day++) {
-            const dayOfWeek = (firstDayIndex + day - 1) % 7;
-            const isSunday = dayOfWeek === 0;
-            const isToday = currentDate.getDate() === today.getDate() && currentDate.getMonth() === today.getMonth() && currentDate.getFullYear() === today.getFullYear();
-            const isSelected = currentDate.toDateString() === selectedDate.toDateString();
+        const tdClass = isSunday ? "sunday" : "";
+        const tdId = isToday ? "today-month-date" : "";
+        const selectedTdClass = isSelected ? "selected-date" : "";
 
-            const tdClass = isSunday ? "sunday" : "";
-            const tdId = isToday ? "today-month-date" : "";
-            const selectedTdClass = isSelected ? "selected-date" : "";
-
-            dayCells.push(
-                <td
-                    key={day}
-                    className={`${tdClass} ${selectedTdClass} ${this.isHoliday(day, currentMonthIndex, currentYear) ? 'holiday' : ''}`}
-                    id={tdId}
-                    onClick={() => this.handleDateClick(day, currentMonthIndex, currentYear)}
-                >
-                    <span>{day}</span>
-                </td>
-            );
-
-            currentDate.setDate(currentDate.getDate() + 1);
-
-            if ((firstDayIndex + day) % 7 === 0) {
-                calendarRows.push(<tr key={day}>{dayCells}</tr>);
-                dayCells = [];
-            }
-        }
-
-        if (dayCells.length > 0) {
-            while (dayCells.length < 7) {
-                dayCells.push(<td key={dayCells.length}></td>);
-            }
-            calendarRows.push(<tr key="last">{dayCells}</tr>);
-        }
-
-        const formattedSelectedDate = `${selectedDate.toLocaleString('en-in', { weekday: 'long' })} ${selectedDate.getDate()}/${selectedDate.getMonth() + 1}/${selectedDate.getFullYear()}`;
-
-        return (
-            <>
-                <div className="month-calendar-container">
-                    <div className="navigation">
-                        <div className="navigation-year">
-                            <button onClick={showPrevYear}>&lt;</button>
-                            <span id="year-display">{currentYear}</span>
-                            <button onClick={showNextYear}>&gt;</button>
-                        </div>
-                        <div className="navigation-month">
-                            <button onClick={showPrevMonth}>&lt;</button>
-                            <span id="month-display">{monthNames[currentMonthIndex]}</span>
-                            <button onClick={showNextMonth}>&gt;</button>
-                        </div>
-                    </div>
-                    <table>
-                        <thead>
-                            <tr>
-                                <th>Sun</th><th>Mon</th><th>Tue</th><th>Wed</th><th>Thu</th><th>Fri</th><th>Sat</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {calendarRows}
-                        </tbody>
-                    </table>
-                </div>
-                <hr />
-                <div className="event-container">
-                    <div className="date-and-day">{formattedSelectedDate}</div>
-                    <div className="event-name">No Events</div>
-                </div>
-            </>
+        dayCells.push(
+            <td
+                key={day}
+                className={`${tdClass} ${selectedTdClass} ${isHoliday(day, currentMonth, currentYear) ? 'holiday' : ''}`}
+                id={tdId}
+                onClick={() => handleDateClick(day, currentMonth, currentYear)}
+            >
+                <span id='day'>{day}</span>
+                {/* {day} */}
+            </td>
         );
+
+        currentDate.setDate(currentDate.getDate() + 1);
+
+        if ((firstDayIndex + day) % 7 === 0) {
+            calendarRows.push(<tr key={day}>{dayCells}</tr>);
+            dayCells = [];
+        }
     }
+
+    if (dayCells.length > 0) {
+        while (dayCells.length < 7) {
+            dayCells.push(<td key={dayCells.length}></td>);
+        }
+        calendarRows.push(<tr key="last">{dayCells}</tr>);
+    }
+
+    const formattedSelectedDate = `${selectedDate.toLocaleString('en-in', { weekday: 'long' })} ${selectedDate.getDate()}/${selectedDate.getMonth() + 1}/${selectedDate.getFullYear()}`;
+
+    return (
+        <>
+            <div className="month-calendar-container">
+                <div className="navigation">
+                    <div className="navigation-year">
+                        <button onClick={showPrevYear}>&lt;</button>
+                        <span id="year-display">{currentYear}</span>
+                        <button onClick={showNextYear}>&gt;</button>
+                    </div>
+                    <div className="navigation-month">
+                        <button onClick={showPrevMonth}>&lt;</button>
+                        <span id="month-display">{monthNames[currentMonth]}</span>
+                        <button onClick={showNextMonth}>&gt;</button>
+                    </div>
+                </div>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Sun</th><th>Mon</th><th>Tue</th><th>Wed</th><th>Thu</th><th>Fri</th><th>Sat</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {calendarRows}
+                    </tbody>
+                </table>
+            </div>
+            <hr />
+            <div className="event-container">
+                <div className="date-and-day">{formattedSelectedDate}</div>
+                <div className="event-name">No Events</div>
+            </div>
+        </>
+    );
 }
 
 export default GenerateMonthlyCalendar;
-
